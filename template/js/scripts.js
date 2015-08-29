91ef594d20fca299a8f710fa8ead9856f7b96758
Ron Daryl Magno <ron.zohan@gmail.com>
2015-08-29 12:54:26 +0800

    Add routing of page

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -1,15 +1,34 @@
-var app = angular.module('techni',[]);
+var app = angular.module('techni',['ngRoute']);
 
-app.controller('buildThumbnailController', function($scope) {
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl : 'pages/imageGallery.html',
+            controller  : 'imageGalleryController'
+        })
+
+        .when('/:imageID/', {
+            templateUrl : 'pages/imageDetails.html',
+            controller  : 'imageDetailsController'
+        })
+
+});
+
+app.controller('imageGalleryController', function($scope) {
     $scope.images = [
         {imageID:0, title:'Mona Lisa', author:'Leonardo da Vinci', dateUpload:'August 20, 2015'},
         {imageID:1, title:'The Scream', author:'Edvard Munch', dateUpload:'August 20, 2015'},
         {imageID:2, title:'The Starry Night', author:'Vincent van Gogh', dateUpload:'August 20, 2015'},
-        {imageID:3, title:'The Starry Night', author:'Vincent van Gogh', dateUpload:'August 20, 2015'}
+        {imageID:3, title:'The Starry Night', author:'Vincent van Gogh', dateUpload:'August 20, 2015'},
+         {imageID:3, title:'The Starry Night', author:'Vincent van Gogh', dateUpload:'August 20, 2015'},
+          {imageID:3, title:'The Starry Night', author:'Vincent van Gogh', dateUpload:'August 20, 2015'}
+          , {imageID:3, title:'The Starry Night', author:'Vincent van Gogh', dateUpload:'August 20, 2015'}
+          , {imageID:3, title:'The Starry Night', author:'Vincent van Gogh', dateUpload:'August 20, 2015'}
     ];
 });
 
-app.controller('viewImageDetailController', function($scope) {
+app.controller('imageDetailsController',['$scope', '$routeParams', function($scope, $routeParams) {
+    $scope.imageID = $routeParams.imageID;
 
+}]);
 
-});
